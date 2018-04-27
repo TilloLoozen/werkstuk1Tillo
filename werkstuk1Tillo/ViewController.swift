92813b6af539e1641c9350187c10d7571ec02d3a
LOOZEN Tillo (s) <tillo.loozen@dt-c205-24.local>
2018-04-27 14:04:46 +0200

    Initial Commit

---
+++ werkstuk1Tillo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  werkstuk1Tillo
+//
+//  Created by LOOZEN Tillo (s) on 27/04/2018.
+//  Copyright © 2018 LOOZEN Tillo (s). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
